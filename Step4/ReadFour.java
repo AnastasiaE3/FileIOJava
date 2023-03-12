ff3f45314686babb29932ae062d5022e67399548
AnastasiaE3 <125825396+AnastasiaE3@users.noreply.github.com>
2023-03-12 15:25:04 -0400

    worked on readFour

--- Step4/ReadFour.java
+++ Step4/ReadFour.java
@@ -1,5 +1,6 @@
 package Step4;
 
+import javax.sound.sampled.Line;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
@@ -14,5 +15,26 @@ public class ReadFour
         {
             // Print out a running total of all the
             // values in the input file.
+
+            try {
+                Scanner fileIn = new Scanner(new File("input.txt"));
+                int sum = 0;
+                while (fileIn.hasNext())
+                {
+                    // Reads the entire line as a string
+                    String lineIn = fileIn.nextLine();
+                    System.out.println(lineIn);
+                    // Split the line into a String array
+                    String [] input = lineIn.split(" , ");
+                    int i;
+                    for ( i = 0; i < input.length; i++);
+                        sum = sum + Integer.parseInt(input[i]);
+                    }
+                    System.out.println(sum);
+                fileIn.close();
+            }
+            catch (IOException e) {
+                System.out.println("File not found");
         }
+     }
 }
